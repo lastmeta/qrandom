6cd463958c86336d0d27c1576d2e83128c2eae75
Jordan Miller <jordan.kay@gmail.com>
2018-11-14 23:26:47 -0700

    commit

--- qrandom/lib/TrueRNG.py
+++ qrandom/lib/TrueRNG.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+'''
 # TrueRNG Read - Simple Example
 # Chris K Cockrum
 # 8/21/2016
@@ -9,116 +9,137 @@
 #
 # Python 2.7.xx is available here: https://www.python.org/
 # Install Pyserial package with:   python -m pip install pyserial
+'''
 
 import serial
 import time
+import numpy as np
 from bitstring import BitArray
 from serial.tools import list_ports
 
 class TrueRNG(object):
-    """docstring for [object Object]."""
-    def __init__(self, blocksize=102400, numloops=10, rng_com_port='default'):
-        self.blocksize = blocksize
-        self.numloops = numloops
+    """ TrueRNG object for reading random bits from the TrueRNG 3 USB device """
 
+    def __init__(self, blocksize=1024, blocksize_type='bytes', return_type='string', port='default'):
+        self.set_blocksize(blocksize)
+        self.set_blocksize_type(blocksize_type)
+        self.set_return_type(return_type)
+        self.set_port(port)
+
+    # sets #####################################################################
+
+    def set_blocksize(self, blocksize):
+        self.blocksize = blocksize
+        return self.blocksize
+
+    def set_blocksize_type(self, blocksize_type):
+        self.blocksize_type = blocksize_type
+        if blocksize_type.startswith('b'):
+            self.blocksize_modulator = 1
+        elif blocksize_type.startswith('k'):
+            self.blocksize_modulator = 1024
+        elif blocksize_type.startswith('m'):
+            self.blocksize_modulator = 1024*1024
+        else:
+            raise 'invalid blocksize_type. options include: [bytes, kilobytes, megabytes]'
+        return self.blocksize_type
+
+    def set_return_type(self, return_type='string'):
+        self.return_type = return_type
+        if not (return_type.startswith('s')
+        or blocksize_type.startswith('b')
+        or blocksize_type.startswith('a')):
+            raise 'invalid return_type. options include: [string, bits, array]'
+        return self.return_type
+
+    def set_port(self, rng_com_port='default'):
         def _look_for_port(available_port, name="TrueRNG"):
             rng_com_port = None
             for temp in ports_avaiable:
                 if temp[1].startswith(name):
-                    if rng_com_port == None:        # always chooses the 1st TrueRNG found
+                    # always chooses the 1st TrueRNG found
+                    if rng_com_port == None:
                         return str(temp[0]), str(temp[0])
             return None, None
 
-        # Print our header
         if rng_com_port == 'default':
             ports=dict()
             ports_avaiable = list(list_ports.comports())
             rng_com_port = None
             # Loop on all available ports to find TrueRNG
-            rng_com_port, self.rng_com_port = _look_for_port(ports_avaiable, name="TrueRNG")
+            rng_com_port, self.port = _look_for_port(ports_avaiable, name="TrueRNG")
             if rng_com_port == None:
-                rng_com_port, self.rng_com_port = _look_for_port(ports_avaiable, name="USB Serial Device")
+                rng_com_port, self.port = _look_for_port(ports_avaiable, name="USB Serial Device")
         else:
-            self.rng_com_port = rng_com_port
+            self.port = rng_com_port
 
-        # Print which port we're using
-        #print(self.rng_com_port)
+        return self.port
 
+    # gets #####################################################################
 
-    def generate(self):
-        # Try to setup and open the comport
-        try:
-            ser = serial.Serial(port=self.rng_com_port,timeout=10)  # timeout set at 10 seconds in case the read fails
-        except:
-            print('Port Not Usable!')
-            print('Do you have permissions set to read ' + self.rng_com_port + ' ?')
-        # Open the serial port if it isn't open
-        if(ser.isOpen() == False):
-            ser.open()
-        # Set Data Terminal Ready to start flow
-        ser.setDTR(True)
-        # This clears the receive buffer so we aren't using buffered data
-        ser.flushInput()
-        # Keep track of total bytes read
-        totalbytes=0
-        bytes = []
-        # Loop
-        for _ in range(self.numloops):
+    def get_blocksize(self):
+        return self.blocksize
 
-            # Try to read the port and record the time before and after
-            try:
-                #before = time.time()    # in microseconds
-                x=ser.read(self.blocksize)   # read bytes from serial port
-                #after = time.time()     # in microseconds
-            except:
-                print('Read Failed!!!')
-                break
+    def get_blocksize_type(self):
+        return self.blocksize_type
 
-            # Update total bytes read
-            totalbytes += len(x)
+    def get_port(self):
+        return self.port
 
-            # If we were able to open the file, write to disk
-            bx = BitArray(x).bin
-            bytes.append(bx)
+    def get_return_type(self):
+        return self.return_type
 
-            # Calculate the rate
-            #rate=float(self.blocksize) / ((after-before)*1000.0)
+    # generate #################################################################
 
-            #print(str(totalbytes) + ' Bytes Read at ' + '{:6.2f}'.format(rate) + ' Kbytes/s')
+    def generate(self, blocksize=None, return_type=None):
+        ''' generate and return a bits as bits, string or array '''
+        blocksize = blocksize or self.blocksize
+        return_type = return_type or self.return_type
 
-        # Close the serial port
-        ser.close()
-
-        #bits = []
-        #for byte in bytes:
-        #    bit = BitArray(byte)
-        #    bits.append(bit.bin)
-
-        return bytes
-
-
-    def generate_bits(self):
         # Try to setup and open the comport
         try:
             # timeout set at 10 seconds in case the read fails
-            ser = serial.Serial(port=self.rng_com_port, timeout=10)
+            ser = serial.Serial(port=self.port, timeout=10)
         except:
             print('Port Not Usable!')
-            print('Do you have permissions set to read ' + self.rng_com_port + ' ?')
+            print(f'Do you have permissions set to read {self.port}?')
+
         # Open the serial port if it isn't open
-        if ser.isOpen() is False:
+        if(ser.isOpen() == False):
             ser.open()
+
         # Set Data Terminal Ready to start flow
         ser.setDTR(True)
+
         # This clears the receive buffer so we aren't using buffered data
         ser.flushInput()
-        # Keep track of total bytes read
-        bits = ser.read(self.blocksize)   # read bytes from serial port
-        bits = BitArray(bits).bin
-        # Close the serial port
-        ser.close()
-        return bits
 
-#a = TrueRNG(102400, 1)
-#b = a.generate()
-#print(b)
+        # Try to read the port
+        success = False
+        y = 0
+        while not success and y < 10:
+            try:
+                bits = ser.read(blocksize * self.blocksize_modulator)
+                success = True
+            except:
+                print('Read Failed!!!')
+                y += 1
+
+        ser.close()
+
+        # convery if required
+        if return_type == 'bits':
+            return bits
+
+        # If we were able to open the file, write to disk
+        bytes = BitArray(bits).bin
+
+        if return_type == 'array':
+            return self.convert_to_array(bytes)
+        # as string
+        return bytes
+
+
+    @staticmethod
+    def convert_to_array(bitstring: str) -> np.ndarray:
+        return np.fromstring(bitstring, 'u1') - ord('0')
